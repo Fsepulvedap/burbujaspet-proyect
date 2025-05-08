fed3e5c8c09fc0a59fce8e72ca4ee2797a3dd3d0
Fernando Sepúlveda <fsepulveda.87@gmail.com>
2025-05-08 00:12:19 +0000

    incio de proyecto

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -1,13 +1,37 @@
 import { Link } from "react-router-dom";
+import LOGO_01 from "./LOGO_01.png"
 
 export const Navbar = () => {
 
 	return (
 		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
+			<div className="container-fluid">
+				<a className="navbar-brand" href="/">
+					<img
+						src={LOGO_01}
+						alt="Our Logo"
+						style={{
+							width: "235px",
+							height: "97px",
+						}}
+					/>
+				</a>
+				<div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+    </div>
 				<div className="ml-auto">
 					<Link to="/demo">
 						<button className="btn btn-primary">Check the Context in action</button>
