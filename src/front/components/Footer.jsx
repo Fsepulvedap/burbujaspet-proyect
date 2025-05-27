3dfa57490015c5339811104f3e1f62f3c5c57901
Fernando Sepúlveda <fsepulveda.87@gmail.com>
2025-05-27 01:16:32 +0000

    comienzo footer

--- src/front/components/Footer.jsx
+++ src/front/components/Footer.jsx
@@ -1,11 +1,17 @@
+import logoFooter from "../assets/img/logoFooter.png"
+
+
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Check the <a target="_blank" href="https://4geeks.com/docs/start/react-flask-template">template documentation</a> <i className="fa-solid fa-file"></i> for help.
-		</p>
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
+		<img 
+			src={logoFooter} 
+			className="logoFooter" 
+			alt="..." 
+			style={{					
+				width: "141px",
+				height: "197px",
+				
+			}}
+		/>
 	</footer>
 );
